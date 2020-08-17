62f2137723049e82ae7815cc5f167ae17acd02d8
chenggong602355 <chenggong602355@pwrd.com>
2020-08-17 19:44:39 +0800

    初始化

--- src/main/java/com/cg/house_keeping/dao/UserRepository.java
+++ src/main/java/com/cg/house_keeping/dao/UserRepository.java
@@ -3,7 +3,6 @@ package com.cg.house_keeping.dao;
 import com.cg.house_keeping.model.po.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
